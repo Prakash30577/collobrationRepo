99fdf93c581a14074f8ba3b523aecaa5717d076b
Omprakash <pmmewada46@gmail.com>
2023-06-06 17:50:41 +0530

    Fix the isssue.

--- src/com/test/PracticeLogics2.java
+++ src/com/test/PracticeLogics2.java
@@ -5,6 +5,8 @@ public class PracticeLogics2 {
 	public static void main(String[] args) {
 		
 		System.out.println("Need to Update All project Code");
+		
+		System.out.println("Do some changes");
 
 	}
 
